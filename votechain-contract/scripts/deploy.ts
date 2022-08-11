060b32c88f520d853136f884b792bb1ef02fe321
Cheksman <ogagaochekwu01@gmail.com>
2022-08-11 07:08:34 +0100

    added function to deploy and verify contracts

--- votechain-contract/scripts/deploy.ts
+++ votechain-contract/scripts/deploy.ts
@@ -1,20 +1,29 @@
-import { ethers } from "hardhat";
+import { ethers, run, network } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  const VoteChaninFactory = await ethers.getContractFactory("VoteChain");
+  const voteChain = await VoteChaninFactory.deploy();
+  await voteChain.deployed();
+  if(network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    await voteChain.deployTransaction.wait(5)
+    await verify(voteChain.address, [])
+  }
 }
 
+async function verify(contractAddress: string, args: any) {
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (ex: any) {
+    if(ex.message.toLowerCase().includes("already verified")){
+      console.log("Already Verified")
+    } else {
+      console.log(ex)
+    }
+  }
+}
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
